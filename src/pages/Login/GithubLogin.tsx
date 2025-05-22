3c573f5e46c7f1a9574cba3b2d89b2901b73e538
Gabriel A Lopes <g.a.lopes201@outlook.com>
2025-05-22 13:50:56 -0300

    correção do botão com falha (github abrindo sem clicar no botão)

--- src/pages/Login/GithubLogin.tsx
+++ src/pages/Login/GithubLogin.tsx
@@ -1,14 +1,16 @@
 import { Button } from "@chakra-ui/react";
 
 //link de autenticação
-const GITHUB_AUTH_URL = "http://localhost:3001/auth/github";
+const GITHUB_AUTH_URL = "http://localhost:3000/auth/github";
 
 //Redireciona o usuário para a rota de autenticação do GitHub
 export function CustomGithubButton(){
-  window.location.href = GITHUB_AUTH_URL;
+  const handleLogin = () => {
+    window.location.href = GITHUB_AUTH_URL;
+  };
 //retorna o botão pronto
   return(
-    <Button id="github-icon-in" onClick={() => CustomGithubButton()} >
+    <Button id="github-icon-in" onClick={handleLogin} >
         <i className="fa-brands fa-github"/>
     </Button>
   )
